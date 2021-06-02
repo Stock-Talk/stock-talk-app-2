7dc3bbe22e9e7f130dc6f40d1d2a0f9c7d1f4d57
Idzel Jaimes <irjaimes@outlook.com>
2021-06-01 21:05:45 -0700

    added postform to capture post info

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Form, TextArea, Modal, Button } from 'semantic-ui-react';
+
+function PostForm() {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={<Button>Create a Post!</Button>}
+    >
+      <Modal.Header>Create a Post</Modal.Header>
+      <Modal.Content Form>
+        <Form onSubmit='undefined'>
+          <Form.Group>
+            <TextArea
+              placeholder='What do you want to share?'
+              name='postText'
+              value=''
+              onChange='undefined'
+              style={{ minHeight: 100 }}
+            />
+
+            <Form.Button color='green' content='Submit' />
+          </Form.Group>
+        </Form>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='gray' onClick={() => setOpen(false)}>
+          Close
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+}
+
+export default PostForm;
